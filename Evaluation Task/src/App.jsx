810886432eb09b54d57e5af035d235e85764c7c5
MD SAMIULLAH <sami.mohd29@gmail.com>
2024-07-19 20:43:41 +0530

    Remove a line.

--- Evaluation Task/src/App.jsx
+++ Evaluation Task/src/App.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Layout from "./Layout";
 import About from "./components/About/About";
