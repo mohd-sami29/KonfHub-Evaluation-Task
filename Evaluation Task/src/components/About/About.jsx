9bb5e4f1120f0e8306a99bc25c51f51c106161c6
MD SAMIULLAH <sami.mohd29@gmail.com>
2024-07-19 20:10:45 +0530

    Modify component add data from api.

--- Evaluation Task/src/components/About/About.jsx
+++ Evaluation Task/src/components/About/About.jsx
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import useApi from "../Api/useApi";
+import DOMPurify from "dompurify";
 
 function About() {
   const { data, error } = useApi();
+  const [htmlContent, setHtmlContent] = useState("");
+
+  useEffect(() => {
+    if (data && data.description) {
+      const htmlString = data.description;
+      const sanitizedHtmlString = DOMPurify.sanitize(htmlString);
+      setHtmlContent(sanitizedHtmlString);
+    }
+  }, [data]);
+
+  if (error) {
+    return (
+      <div className="flex justify-center items-center p-4">
+        <p className="text-red-500 text-2xl font-semibold p-4 rounded border border-slate-500">
+          Error: {error.message}
+        </p>
+      </div>
+    );
+  }
+
   return (
     <>
-      <h2>ABOUT EVENT</h2>
+      <div dangerouslySetInnerHTML={{ __html: htmlContent }} />
     </>
   );
 }
