e16bbe0d247c58d6993e1f41a979f4f95c83c949
MD SAMIULLAH <sami.mohd29@gmail.com>
2024-07-19 16:41:53 +0530

    Built App component.

---
+++ Evaluation Task/src/App.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Layout from "./Layout";
+import About from "./components/About/About";
+import Home from "./components/Home/Home";
+import Tickets from "./components/Tickets/Tickets";
+import Speakers from "./components/Speakers/Speakers";
+import Workshop from "./components/Workshop/Workshop";
+import Sponsers from "./components/Sponsers/Sponsers";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { path: "", element: <Home /> },
+      { path: "/about", element: <About /> },
+      { path: "/tickets", element: <Tickets /> },
+      { path: "/speakers", element: <Speakers /> },
+      { path: "/workshop", element: <Workshop /> },
+      { path: "/sponsers", element: <Sponsers /> },
+    ],
+  },
+]);
+
+function App() {
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
+}
+
+export default App;
