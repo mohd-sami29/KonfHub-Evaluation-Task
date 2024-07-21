e01483243e1ec5df5697e74890838e14f4a4e142
MD SAMIULLAH <sami.mohd29@gmail.com>
2024-07-21 20:19:58 +0530

    improve path

--- Evaluation Task/src/components/Header/Header.jsx
+++ Evaluation Task/src/components/Header/Header.jsx
@@ -98,7 +98,7 @@ export default function Header() {
               </li>
               <li>
                 <NavLink
-                  to="/sponsors"
+                  to="/sponsers"
                   className={({ isActive }) =>
                     `block py-2 pr-4 pl-3 duration-200 ${
                       isActive ? "text-orange-700" : "text-gray-700"
